e2abe9d3fab80adc82c6fd064a47b243c6108a70
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-14 00:18:03 +0300

    define the color scheme and general config

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:doto/pages/home_page.dart';
+import 'package:doto/pages/splash_page.dart';
 import 'package:flutter/material.dart';
 
-import 'pages/home_page.dart';
+import 'utils/constants.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,13 +14,37 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const ColorScheme colorScheme = ColorScheme(
+      primary: primaryColor,
+      secondary: secondaryColor,
+      surface: Color(0xffffffff),
+      background: Color(0xffe0e0e0),
+      error: Color(0xffd32f2f),
+      onPrimary: Color(0xffffffff),
+      onSecondary: Color(0xffffffff),
+      onSurface: Color(0xff000000),
+      onBackground: Color(0xff000000),
+      onError: Color(0xffffffff),
+      brightness: Brightness.light,
+    );
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'DoTo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+          colorScheme: colorScheme,
+          textTheme: TextTheme(
+            titleMedium: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+              color: colorScheme.onPrimary,
+            ),
+            titleSmall: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: colorScheme.onPrimary,
+            ),
+          )),
       home: const MyHomePage(),
     );
   }
 }
-
