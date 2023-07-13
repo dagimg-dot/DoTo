dcfaada9d6a2e7ce4f72c3c8743f9845d150ccdf
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-13 23:22:38 +0300

    app bar and basic body finished

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,10 @@
+import 'package:doto/models/task.dart';
 import 'package:flutter/material.dart';
 
+import '../utils/constants.dart';
+
 class MyHomePage extends StatefulWidget {
-const MyHomePage({super.key}); 
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -19,30 +22,67 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primaryColor,
       appBar: AppBar(
-        title: Text(
-          'Flutter Demo Home Page',
-          style: Theme.of(context).textTheme.headlineMedium,
-        ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: ListTile(
+          leading: Container(
+            width: 60,
+            height: 60,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: secondaryColor,
+                width: 2.0,
+              ),
+              image: const DecorationImage(
+                image: AssetImage('assets/images/profile.jpg'),
+                fit: BoxFit.fill,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          title: const Text('Hello, Dagim'),
+          subtitle: const Text(
+            'Manage your tasks !',
+            style: TextStyle(
+              color: Colors.white
             ),
-          ],
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.more_vert),
+            color: Colors.white,
+            onPressed: () {},
+          ),
         ),
+        toolbarHeight: MediaQuery.of(context).devicePixelRatio * 43.0,
+        elevation: 0.0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: ClipRRect(
+        borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: 15,
+                blurRadius: 20,
+                offset: const Offset(0, 0), // changes position of shadow
+              ),
+              BoxShadow(
+                color: Colors.black.withOpacity(0.5),
+                spreadRadius: -15,
+                blurRadius: 20,
+                offset: const Offset(0, 0), // changes position of shadow
+              )
+            ],
+          ),
+          child: const Center(
+            child: Text(
+              'App Content',
+            ),
+          ),
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
