6774da1cc87886f377ee6f2f48260db53b7bd6ca
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-13 21:46:23 +0300

    create the splash page

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,23 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:doto/pages/home_page.dart';
+import 'package:doto/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage> {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      backgroundColor: primaryColor,
+      splash: Image.asset('assets/icons/splash.png'),
+      splashTransition: SplashTransition.fadeTransition,
+      nextScreen: const MyHomePage(),
+    );
+  }
+}
