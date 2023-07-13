1bacc68e05c1b3729dce83728ead694cb3d25049
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-14 00:17:48 +0300

    impelement basic UI components

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:doto/models/task.dart';
 import 'package:flutter/material.dart';
 
 import '../utils/constants.dart';
@@ -11,14 +10,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -43,9 +34,7 @@ class _MyHomePageState extends State<MyHomePage> {
           title: const Text('Hello, Dagim'),
           subtitle: const Text(
             'Manage your tasks !',
-            style: TextStyle(
-              color: Colors.white
-            ),
+            style: TextStyle(color: Colors.white),
           ),
           trailing: IconButton(
             icon: const Icon(Icons.more_vert),
@@ -53,37 +42,225 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: () {},
           ),
         ),
-        toolbarHeight: MediaQuery.of(context).devicePixelRatio * 43.0,
+        toolbarHeight: MediaQuery.of(context).devicePixelRatio * 40.0,
         elevation: 0.0,
       ),
-      body: ClipRRect(
-        borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(40), topRight: Radius.circular(40)),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                spreadRadius: 15,
-                blurRadius: 20,
-                offset: const Offset(0, 0), // changes position of shadow
+      body: Container(
+        margin: const EdgeInsets.only(top: 9.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40),
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 4,
+              blurRadius: 4,
+              offset: const Offset(0, 0), // changes position of shadow
+            ),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+              topLeft: Radius.circular(40), topRight: Radius.circular(40)),
+          child: Column(
+            children: [
+              // horizontal list view for chips of categories like all, pending, completed and cancelled
+              Container(
+                height: 60,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
+                    // all
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 20.0, left: 20.0, right: 20.0),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      decoration: BoxDecoration(
+                        color: secondaryColor,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: const Text(
+                        'All',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    // pending
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 20.0, left: 20.0, right: 20.0),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            spreadRadius: 4,
+                            blurRadius: 4,
+                            offset: const Offset(
+                                0, 0), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: const Text(
+                        'Pending',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    // completed
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 20.0, left: 20.0, right: 20.0),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            spreadRadius: 4,
+                            blurRadius: 4,
+                            offset: const Offset(
+                                0, 0), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: const Text(
+                        'Completed',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    // cancelled
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 20.0, left: 20.0, right: 20.0),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            spreadRadius: 4,
+                            blurRadius: 4,
+                            offset: const Offset(
+                                0, 0), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: const Text(
+                        'Cancelled',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              BoxShadow(
-                color: Colors.black.withOpacity(0.5),
-                spreadRadius: -15,
-                blurRadius: 20,
-                offset: const Offset(0, 0), // changes position of shadow
-              )
+              Expanded(
+                child: ListView(
+                  children: [
+                    // task 1
+                    Container(
+                      margin: const EdgeInsets.only(
+                          top: 20.0, left: 20.0, right: 20.0),
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.1),
+                            spreadRadius: 4,
+                            blurRadius: 4,
+                            offset: const Offset(
+                                0, 0), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                      child: Row(
+                        children: [
+                          // checkbox
+                          Container(
+                            width: 20,
+                            height: 20,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: secondaryColor,
+                                width: 2.0,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(width: 10.0),
+                          // task title
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: const [
+                                Text(
+                                  'Task 1',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                SizedBox(height: 5.0),
+                                Text(
+                                  'This is task 1',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          // task time
+                          const Text(
+                            '10:00 AM',
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    // task 2
+                  ],
+                ),
+              ),
             ],
           ),
-          child: const Center(
-            child: Text(
-              'App Content',
-            ),
-          ),
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
