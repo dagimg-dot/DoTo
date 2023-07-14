e5ba15798011bb7a35e5fda1615a131f2fba67c8
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-14 23:42:09 +0300

    implement fetch task and filter task functinality

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,7 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
+import '../dao/task_dao_impl.dart';
+import '../models/task.dart';
 import '../utils/constants.dart';
 import '../widgets/horizontal_chip_list.dart';
 
@@ -13,6 +13,44 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Task> tasks = [];
+
+  filterTasks(int filter) async {
+    List<Task> fetchedTasks = await TaskDAOImpl().getAllTasks();
+    List<Task> filteredTasks = [];
+    switch (filter) {
+      case 0:
+        filteredTasks = fetchedTasks;
+        break;
+      case 1:
+        filteredTasks = fetchedTasks
+            .where((task) => task.status == TaskStatus.PENDING)
+            .toList();
+        break;
+      case 2:
+        filteredTasks = fetchedTasks
+            .where((task) => task.status == TaskStatus.COMPLETED)
+            .toList();
+        break;
+      case 3:
+        filteredTasks = fetchedTasks
+            .where((task) => task.status == TaskStatus.CANCELLED)
+            .toList();
+        break;
+      default:
+        filteredTasks = fetchedTasks;
+    }
+    setState(() {
+      tasks = filteredTasks;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    filterTasks(0); // get all tasks when app starts
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -46,15 +84,15 @@ class _HomePageState extends State<HomePage> {
                 "Cancelled",
               ],
               onTap: (index) {
-                log("$index");
+                filterTasks(index);
               },
             ),
           ),
           Expanded(
             child: ListView.builder(
-              itemCount: 10,
+              itemCount: tasks.length,
               itemBuilder: (context, index) {
-                return taskItem();
+                return taskItem(tasks[index]);
               },
             ),
           ),
@@ -64,7 +102,7 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-Container taskItem() {
+Container taskItem(Task task) {
   return Container(
     margin: const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
     padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
@@ -99,19 +137,19 @@ Container taskItem() {
         Expanded(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
+            children: [
               Text(
-                'Task 1',
-                style: TextStyle(
+                task.name,
+                style: const TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.w500,
                   color: Colors.black,
                 ),
               ),
-              SizedBox(height: 5.0),
+              const SizedBox(height: 5.0),
               Text(
-                'This is task 1',
-                style: TextStyle(
+                task.description,
+                style: const TextStyle(
                   fontSize: 14,
                   fontWeight: FontWeight.w400,
                   color: Colors.grey,
