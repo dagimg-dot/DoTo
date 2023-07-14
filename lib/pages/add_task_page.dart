794df8eafcb96dcb53ac16435d12c55f43e2dc91
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-07-14 16:49:29 +0300

    basic page for page navigation trial

---
+++ lib/pages/add_task_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AddTaskPage extends StatefulWidget {
+  const AddTaskPage({super.key});
+
+  @override
+  State<AddTaskPage> createState() => _AddTaskPageState();
+}
+
+class _AddTaskPageState extends State<AddTaskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 9),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(40),
+          topRight: Radius.circular(40),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            spreadRadius: 4,
+            blurRadius: 4,
+            offset: const Offset(0, 0), // changes position of shadow
+          ),
+        ],
+      ),
+      child: const Center(
+        child: Text('Add Task Page'),
+      ),
+    );
+  }
+}
