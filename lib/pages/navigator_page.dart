7fa2085342b8baeee66cef6ba473ae29ce313a03
Dagim G. Astatkie <workflow.jd@gmail.com>
2024-06-22 13:36:34 +0300

    feat: add settings page

--- lib/pages/navigator_page.dart
+++ lib/pages/navigator_page.dart
@@ -1,6 +1,7 @@
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:doto/pages/home_page.dart';
 import 'package:doto/pages/login_page.dart';
+import 'package:doto/pages/settings_page.dart';
 import 'package:doto/services/auth_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +21,7 @@ class _NavigatorPageState extends State<NavigatorPage> {
   final List<Widget> _pages = const [
     HomePage(),
     AddTaskPage(),
-    HomePage(),
+    SettingsPage(),
   ];
 
   @override
